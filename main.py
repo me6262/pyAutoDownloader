3fc17e76ceadf48515dd0b43c8a147fc651497bd
me6262 <hayd6262@gmail.com>
2022-12-06 20:39:25 -0500

    lots and lots of changes

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import gi
 
 # TODO: add comments everywhere
 gi.require_version("Gtk", "3.0")
-from gi.repository import Gtk, Gdk, GdkPixbuf, Gio
+from gi.repository import Gtk, Gdk
 
 (TARGET_ENTRY_TEXT, TARGET_ENTRY_PIXBUF) = range(2)
 (COLUMN_TEXT, COLUMN_PIXBUF) = range(2)
@@ -16,91 +16,31 @@ amode = json.load(a)
 
 current_mode = []
 commands = []
-autos = []
 selected_command = 0
 for i in command:
     commands.append((i["Command"][0]["Name"], i["Command"][1]["params"][0]))
-for i in amode["AutonomousModes"]:
-    autos.append((i["Name"]))
 class DragDropWindow(Gtk.Window):
     def __init__(self):
         super().__init__(title="Autonomous Mode Builder")
         self.selected_auto = 0
         self.set_border_width(10)
         
+        # self.add(bar)
+        
+        self.autos = []
+        for i in amode["AutonomousModes"]:
+            self.autos.append((i["Name"]))
         self.grid = Gtk.Grid()
         self.grid.set_column_homogeneous(True)
         self.grid.set_row_homogeneous(True)
         self.add(self.grid) 
 
+        self.saveButton = Gtk.Button(label="Save")
+        self.loadButton = Gtk.Button(label="Load")
+        self.saveButton.connect("clicked", self.on_save_clicked)
         #when the autonmous mode is selected from the list on the right
-        def on_amode_selected(obj, widget):
-            for row in self.paramsListBox:
-                self.paramsListBox.remove(row)
-            print("clicked")
-            print(obj.get_selected_rows()[0].get_children()[0].get_text())
-            mode_location = obj.get_selected_rows()[0].get_index()
-
-            current_mode = amode["AutonomousModes"][mode_location]
-            self.selected_auto = mode_location
-            mode_name = obj.get_selected_rows()[0].get_children()[0].get_text()
-            # kill all of the previous commands in the builder ListBox
-            for row in self.builderListBox.get_children():
-                row.destroy()
-            # add the commands to the builder listbox
-            for i in amode["AutonomousModes"][mode_location]["Commands"]:
-                row = Gtk.ListBoxRow()
-                label = Gtk.Label()
-                label.set_text(i["Name"])
-                row.add(label)
-                self.builderListBox.add(row)
-                self.show_all()
-        # when a command is selected from the Builder listbox we add all of the parameters
-        # of that command to the parameters listbox
-        def on_amode_command_selected(obj, widget):
-            print("clicked")
-            command_location = obj.get_selected_rows()[0].get_index()
-            #print(amode["AutonomousModes"][mode_location])
-            for row in self.paramsListBox.get_children():
-                row.destroy()
-            try:
-                params = amode["AutonomousModes"][self.selected_auto]["Commands"][command_location]["Parameters"]
-
-            except:
-                params = []
-            if params != []:
-                for i in params:
-                    row = Gtk.ListBoxRow()
-                    entry = Gtk.Entry()
-                    entry.set_text(i) 
-                    row.add(entry)
-                    self.paramsListBox.add(row)
-            row = Gtk.ListBoxRow()
-            combo = Gtk.ComboBoxText()
-            parallel_mode = amode["AutonomousModes"][self.selected_auto]["Commands"][command_location]["ParallelType"]
-
-            combo.append_text("Parallel")
-            combo.append_text("Race")
-            combo.append_text("Deadline_Leader")
-            combo.append_text("Deadline_Follower")
-            combo.append_text("None")
-            if parallel_mode != "None":
-                if parallel_mode == "Parallel":
-                    combo.set_active(0)
-                elif parallel_mode == "Race":
-                    combo.set_active(1)
-                elif parallel_mode == "Deadline_Leader":
-                    combo.set_active(2)
-                elif parallel_mode == "Deadline_Follower":
-                    combo.set_active(3)
-            else:
-                combo.set_active(4)
-
-            row.add(combo)
-            self.paramsListBox.add(row)
-            self.show_all()
             
-
+        
         self.amodeFrame = Gtk.Frame(label="Autonomous Modes")
         self.commandFrame = Gtk.Frame(label="Commands")
         self.builderFrame = Gtk.Frame(label="Auto Editor")
@@ -114,40 +54,254 @@ class DragDropWindow(Gtk.Window):
         self.add_command_button = Gtk.Button(label="<-")
         self.command_up_button = Gtk.Button(label="^")
         self.command_down_button = Gtk.Button(label="v")
+        self.remove_command_button = Gtk.Button(label="--")
+        
+        self.add_amode_button = Gtk.Button(label="+")
+        self.rename_amode_button = Gtk.Button(label="*")
 
         for i in commands:
             self.commandListBox.add(Gtk.Label(i[0]))
         self.commandListBox.add(Gtk.Label("Commands"))
-        for i in autos:
+        for i in self.autos:
             row = Gtk.ListBoxRow()
+            row.drag_source_set(Gdk.ModifierType.BUTTON1_MASK, [], Gdk.DragAction.COPY)
             label = Gtk.Label()
             label.set_text(i)
             row.add(label)
             self.amodeListBox.add(row)
-            
+        
         self.amodeFrame.add(self.amodeListBox)
         self.commandFrame.add(self.commandListBox)
         self.builderFrame.add(self.builderListBox)
         self.paramsFrame.add(self.paramsListBox)
 
+        self.add_command_button.connect("clicked", self.on_add_command_clicked)
+        self.command_down_button.connect("clicked", self.on_command_down_clicked)
+        self.command_up_button.connect("clicked", self.on_command_up_clicked)
+        self.remove_command_button.connect("clicked", self.on_remove_command_clicked)
+        self.rename_amode_button.connect("clicked", self.on_rename_amode)
             
-        self.amodeListBox.connect("row-selected", on_amode_selected)
-        self.builderListBox.connect("row-selected", on_amode_command_selected)
+        self.amodeListBox.connect("row-selected", self.on_amode_selected)
+        self.commandListBox.connect("drag-begin", self.on_command_drag_begin)
+        self.builderListBox.connect("row-selected", self.on_amode_command_selected)
+        self.builderListBox.connect("drag-data-received", self.on_amode_command_drag_end)
+        self.add_amode_button.connect("clicked", self.on_add_amode_clicked)
 
         
         self.grid.set_column_spacing(10)
-        self.grid.attach(self.amodeFrame, 4, 0, 4, 24)
-        self.grid.attach(self.builderFrame, 8, 0, 4, 12)
-        self.grid.attach(self.paramsFrame, 8, 12, 4, 12)
-        self.grid.attach(self.add_command_button, 12, 1, 1, 1)
-        self.grid.attach(self.command_up_button, 12, 2, 1, 1)
-        self.grid.attach(self.command_down_button, 12, 3, 1, 1)
-        self.grid.attach(self.commandFrame, 13, 0, 4, 24)
+        self.grid.attach(self.saveButton, 0, 0, 2, 1)
+        self.grid.attach(self.loadButton, 2, 0, 2, 1)
+        self.grid.attach(self.add_amode_button, 4, 2, 1, 1)
+        self.grid.attach(self.rename_amode_button, 4, 3, 1, 1)
+        self.grid.attach(self.amodeFrame, 0, 1, 4, 20)
+        self.grid.attach(self.builderFrame, 5, 1, 4, 10)
+        self.grid.attach(self.paramsFrame, 5, 11, 4, 10)
+        self.grid.attach(self.add_command_button, 9, 2, 1, 1)
+        self.grid.attach(self.command_up_button, 9, 3, 1, 1)
+        self.grid.attach(self.command_down_button, 9, 4, 1, 1)
+        self.grid.attach(self.remove_command_button, 9, 5, 1, 1)
+        self.grid.attach(self.commandFrame, 10, 1, 4, 20)
         
 
         self.show_all()
 
+    def on_amode_selected(self, widget, obj):
+        for row in self.paramsListBox:
+            self.paramsListBox.remove(row)
+        print("clicked")
+        print(obj.get_parent().get_selected_rows()[0].get_children()[0].get_text())
+        mode_location = obj.get_parent().get_selected_rows()[0].get_index()
 
+        self.current_mode = amode["AutonomousModes"][mode_location]
+        self.selected_auto = mode_location
+        mode_name = obj.get_parent().get_selected_rows()[0].get_children()[0].get_text()
+        # kill all of the previous commands in the builder ListBox
+        for row in self.builderListBox.get_children():
+            row.destroy()
+        # add the commands to the builder listbox
+        for i in amode["AutonomousModes"][mode_location]["Commands"]:
+            row = Gtk.ListBoxRow()
+            label = Gtk.Label()
+            label.set_text(i["Name"])
+            row.add(label)
+            row.drag_source_set(Gdk.ModifierType.BUTTON1_MASK, [], Gdk.DragAction.COPY)
+            row.drag_dest_set(Gtk.DestDefaults.ALL, [], Gdk.DragAction.COPY)
+            # row.connect('activate', self.on_rename_amode)
+            self.builderListBox.add(row)
+            self.show_all()
+    # when a command is selected from the Builder listbox we add all of the parameters
+    # of that command to the parameters listbox
+    def on_amode_command_selected(self, widget, obj):
+        print("clicked")
+        self.command_location = obj.get_parent().get_selected_rows()[0].get_index()
+        #print(amode["AutonomousModes"][mode_location])
+        for row in self.paramsListBox.get_children():
+            row.destroy()
+        try:
+            params = amode["AutonomousModes"][self.selected_auto]["Commands"][self.command_location]["Parameters"]
+
+        except:
+            params = []
+        if params != []:
+            for i in params:
+                row = Gtk.ListBoxRow()
+                entry = Gtk.Entry()
+                entry.set_text(i)
+                entry.connect("changed", self.on_command_changed) 
+                row.add(entry)
+                self.paramsListBox.add(row)
+
+                
+        row = Gtk.ListBoxRow()
+        combo = Gtk.ComboBoxText()
+        parallel_mode = amode["AutonomousModes"][self.selected_auto]["Commands"][self.command_location]["ParallelType"]
+
+        combo.append_text("Parallel")
+        combo.append_text("Race")
+        combo.append_text("Deadline_Leader")
+        combo.append_text("Deadline_Follower")
+        combo.append_text("None")
+        if parallel_mode != "None":
+            if parallel_mode == "Parallel":
+                combo.set_active(0)
+            elif parallel_mode == "Race":
+                combo.set_active(1)
+            elif parallel_mode == "Deadline_Leader":
+                combo.set_active(2)
+            elif parallel_mode == "Deadline_Follower":
+                combo.set_active(3)
+        else:
+            combo.set_active(4)
+
+        combo.connect("changed", self.on_command_changed)
+        row.add(combo)
+        self.paramsListBox.add(row)
+        self.show_all()
+
+    def on_command_changed(self, obj):
+        if (type(obj) == Gtk.ComboBoxText):
+            amode["AutonomousModes"][self.selected_auto]["Commands"][self.command_location]["ParallelType"] = obj.get_active_text()
+        else:
+            amode["AutonomousModes"][self.selected_auto]["Commands"][self.command_location]["Parameters"][obj.get_parent().get_index()] = obj.get_text()
+        print(amode["AutonomousModes"][self.selected_auto]["Commands"])
+        # print(obj.get_active_text())
+        print("changed")
+        
+    def on_save_clicked(self, widget):
+        print("save")
+        with open("amode.json", "w") as f:
+            json.dump(amode, f, indent=4)
+
+    def on_add_command_clicked(self, widget):
+        print("add command")
+        print(self.commandListBox.get_selected_rows()[0].get_children()[0].get_text())
+        if command[self.commandListBox.get_selected_rows()[0].get_index()]["Command"][1]["params"] != [""]:
+            amode["AutonomousModes"][self.selected_auto]["Commands"].append({"Name": self.commandListBox.get_selected_rows()[0].get_children()[0].get_text(), "Parameters": command[self.commandListBox.get_selected_rows()[0].get_index()]["Command"][1]["params"], "ParallelType": "None"})
+        else: 
+            amode["AutonomousModes"][self.selected_auto]["Commands"].append({"Name": self.commandListBox.get_selected_rows()[0].get_children()[0].get_text(), "Parameters": [], "ParallelType": "None"})
+        row = Gtk.ListBoxRow()
+        row.add(Gtk.Label(self.commandListBox.get_selected_rows()[0].get_children()[0].get_text()))
+        self.builderListBox.add(row)
+        print(amode["AutonomousModes"][self.selected_auto]["Commands"])
+        self.show_all()
+    
+    def on_command_up_clicked(self, widget):
+        print("up")
+        if self.command_location > 0:
+            temp = amode["AutonomousModes"][self.selected_auto]["Commands"][self.command_location]
+            amode["AutonomousModes"][self.selected_auto]["Commands"][self.command_location] = amode["AutonomousModes"][self.selected_auto]["Commands"][self.command_location - 1]
+            amode["AutonomousModes"][self.selected_auto]["Commands"][self.command_location - 1] = temp
+            self.command_location -= 1
+            for row in self.builderListBox.get_children():
+                row.destroy()
+            for i in amode["AutonomousModes"][self.selected_auto]["Commands"]:
+                row = Gtk.ListBoxRow()
+                label = Gtk.Label()
+                label.set_text(i["Name"])
+                row.add(label)
+                self.builderListBox.add(row)
+                self.show_all()
+            self.builderListBox.select_row(self.builderListBox.get_row_at_index(self.command_location))
+            self.on_amode_command_selected(self, self.builderListBox)
+        
+    def on_command_down_clicked(self, widget):
+        print("down")
+        if self.command_location < len(amode["AutonomousModes"][self.selected_auto]["Commands"]) - 1:
+            temp = amode["AutonomousModes"][self.selected_auto]["Commands"][self.command_location]
+            amode["AutonomousModes"][self.selected_auto]["Commands"][self.command_location] = amode["AutonomousModes"][self.selected_auto]["Commands"][self.command_location + 1]
+            amode["AutonomousModes"][self.selected_auto]["Commands"][self.command_location + 1] = temp
+            self.command_location += 1
+            for row in self.builderListBox.get_children():
+                row.destroy()
+            for i in amode["AutonomousModes"][self.selected_auto]["Commands"]:
+                row = Gtk.ListBoxRow()
+                label = Gtk.Label()
+                label.set_text(i["Name"])
+                row.add(label)
+                self.builderListBox.add(row)
+                self.show_all()
+            self.builderListBox.select_row(self.builderListBox.get_row_at_index(self.command_location))
+            self.on_amode_command_selected(self, widget, obj=self.builderListBox.get_selected_rows()[0])
+    
+    def on_remove_command_clicked(self, widget):
+        print("remove")
+        amode["AutonomousModes"][self.selected_auto]["Commands"].pop(self.command_location)
+        for row in self.builderListBox.get_children():
+            row.destroy()
+        for i in amode["AutonomousModes"][self.selected_auto]["Commands"]:
+            row = Gtk.ListBoxRow()
+            label = Gtk.Label()
+            label.set_text(i["Name"])
+            row.add(label)
+            self.builderListBox.add(row)
+            self.show_all()
+        if self.command_location > 0:
+            self.command_location -= 1
+            self.builderListBox.select_row(self.builderListBox.get_row_at_index(self.command_location))
+            self.on_amode_command_selected(self, self.builderListBox)
+        else:
+            self.paramsListBox.foreach(lambda x: x.destroy())
+            self.show_all()
+    def on_amode_command_drag_end(self, widget, context, x, y, time):
+        print("drag")
+        self.command_location = self.builderListBox.get_row_at_y(y).get_index()
+        row = Gtk.ListBoxRow()
+        row.add(Gtk.Label(command[self.command_location]["Command"][0]))
+        print(self.command_location)
+        self.show_all()
+        
+    def on_command_drag_begin(self, widget, context):
+        print("drag begin")
+        self.command_location = self.commandListBox.get_selected_rows()[0].get_index()
+        row = Gtk.ListBoxRow()
+        row.add(Gtk.Label(command[self.command_location]["Command"][0]))
+        self.show_all()
+    def on_add_amode_clicked(self, widget):
+        print("add amode")
+        amode["AutonomousModes"].append({"Name": "New Mode", "Commands": []})
+        row = Gtk.ListBoxRow()
+        row.add(Gtk.Label("New Mode"))
+        self.amodeListBox.add(row)
+        self.selected_auto = len(amode["AutonomousModes"]) - 1
+        self.show_all()
+        # TODO: make this work
+    def on_rename_amode(self, widget):
+        print("rename amode")
+        dialog = Gtk.Dialog()
+        
+        entry = Gtk.Entry()
+        name = self.amodeListBox.get_selected_rows()[0].get_child().get_text()
+        entry.set_text(name)
+        # dialog.get_child().add(entry)
+        dialog.new()
+        dialog.run()
+        self.show_all()
+        print(name)
+        # obj.get_children()[0].destroy()
+        # renameEntry = Gtk.Entry()
+        # renameEntry.set_text(amode["AutonomousModes"][self.selected_auto]["Name"])
+        # obj.add(renameEntry)
+    
 win = DragDropWindow()
 win.connect("destroy", Gtk.main_quit)
 win.show_all()
